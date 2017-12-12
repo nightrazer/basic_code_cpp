6f235ce9bdb90047c82a4500c43fb694b2695979
nightrazer <nightrazer@gmail.com>
2017-12-12 19:40:13 +0100

    Create Rockpapperscissors.cpp
    I know there is a ton a mistakes here. I will be looking into what has went wrong, but if you want to help out I appreciate it!

---
+++ Rockpapperscissors.cpp
@@ -0,0 +1,44 @@
+// Rock, Paper. Scissors
+
+#include <iostream>
+#include <string>
+using std::string
+using namespace std;
+
+int main(){
+
+string yourchoice;
+string computerchoice;
+
+int (scissors =< rock);
+int (paper >= rock);
+int (scissors >= paper);
+
+	if (yourchoice >= computerchoice) 
+	{
+		cout << "Player wins!" << endl;
+	} else if (yourchoice == computerchoice)
+		{
+			cout << "Tie!" << endl;		
+		} else {
+			cout << "Computer wins!" << endl;
+		}
+
+	cout << "Pick paper, scissors or rock" << endl;
+	cin >> yourchoice;
+	cout << "You picked ";
+	cout >> yourchoice;
+	cout << " " << endl;
+	cout << " " << endl;
+
+	cout << "Computer picked ";
+	cin >> computerchoice;
+	cout << " " << endl;
+	cout << " " << endl;
+
+	cout << "The winner is... ";
+	cin >> endresult;
+
+
+return 0;
+}
